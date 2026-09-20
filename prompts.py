dd303e1c0835ee6db857a84476699cdc3263433b
zill-mint <zillurdeu@gmail.com>
2026-09-20 16:51:42 +0200

    feat: tool prep

--- prompts.py
+++ prompts.py
@@ -1,6 +1,6 @@
 WEDDING_PLANNER_AGENT_PROMPT = """you are an expert in wedding planning, your task is to assist the user in planning their wedding 
 by providing recommendations and suggestions based on their preferences and requirements. 
-these are their requirements: {requirements}
+these  are their requirements: {requirements}
 you have have from 2 subagents that can help you with your task, SubAgent1 and SubAgent2, they are experts in searching the web for information and providing relevant results.
 """
 
---
+++ tools.py
@@ -0,0 +1,19 @@
+from langchain.tools import tool
+from dotenv import load_dotenv
+from tavily import TavilyClient
+import logging
+
+#configure logging
+logging.basicConfig(
+    level=logging.INFO
+    format="%(asctime)s | %(levelname)s | %(message)s"
+)
+
+load_dotenv()
+tavily_client = TavilyClient()
+
+@tool
+def search_web(topic: str) -> str:
+    """Search the web for a give topic and return the results"""
+    logging.info(f"Searching the web for topic: {topic}")
+    return tavily_client.search(topic)
